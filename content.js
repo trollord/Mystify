46224789687c0e8bb29d748002acc8300bae56e6
trollord <mkasangottuwar@clarapath.com>
2023-08-13 11:22:16 +0530

    updats coming soon

---
+++ content.js
@@ -0,0 +1,23 @@
+
+var id = 0;
+
+blurIt = () => {
+    clearTimeout(id);
+
+    let chatList = document.getElementById("side");
+    chatList ? chatList.classList.add("blurrit-class", "blur-10") : "";
+
+    let header = document.getElementsByTagName('header')[1];
+    header ? header.classList.add("blurrit-class", "blur-4") : "";
+
+    for (i = 0; i < 256; ++i) {
+        let potentialListOfUserNameColors = document.getElementsByClassName(`color-${i + 1}`);
+        let potentialListOfUserNameBgColors = document.getElementsByClassName(`bg-color-${i + 1}`);
+        for (item of potentialListOfUserNameColors) { item.classList.add("blurrit-class", "blur-4"); }
+        for (item of potentialListOfUserNameBgColors) { item.classList.add("blurrit-class", "blur-4"); }
+    }
+
+    id = setTimeout(blurIt, 1000);
+}
+
+blurIt();
